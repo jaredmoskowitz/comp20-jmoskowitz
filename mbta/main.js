c9d961b053e9d665f7cd3930890f98ae20c14b52
Jared Moskowitz <jaredmoskowitz@me.com>
2016-07-23 18:26:02 -0700

    got a map showing and centered at south

---
+++ mbta/main.js
@@ -0,0 +1,11 @@
+var map;
+
+function init() {
+  initMap();
+}
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 42.352271, lng: -71.05524200000001},
+    zoom: 8
+  });
+}
